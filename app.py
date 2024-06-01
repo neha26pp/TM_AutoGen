d1c43e815aa4b521e06b9c5cc268942715ce1b67
Neha Pandit <nxp5310@psu.edu>
2024-05-31 17:26:09 -0700

    firebase config

--- app.py
+++ app.py
@@ -14,9 +14,22 @@ import ast
 import autogen_chat_with_syllabus
 import json 
 
+firebase_config = st.secrets["firebase"]
+
 try:
-    # Try to initialize Firebase app
-    cred = json.loads(dict(st.secrets["firebase"]['my_project_settings']))
+    # Create a credentials object
+    cred = credentials.Certificate({
+        "type": firebase_config["type"],
+        "project_id": firebase_config["project_id"],
+        "private_key_id": firebase_config["private_key_id"],
+        "private_key": firebase_config["private_key"].replace('\\n', '\n'),
+        "client_email": firebase_config["client_email"],
+        "client_id": firebase_config["client_id"],
+        "auth_uri": firebase_config["auth_uri"],
+        "token_uri": firebase_config["token_uri"],
+        "auth_provider_x509_cert_url": firebase_config["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": firebase_config["client_x509_cert_url"]
+    })
     firebase_admin.initialize_app(cred, {'storageBucket': 'carl-1667a.appspot.com'})
 except ValueError as e:
     # If app already exists, do nothing
